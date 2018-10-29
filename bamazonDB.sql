eb4e8974a25daac128e644a58fd2b5d494717440
ANS <36783010+answebdev@users.noreply.github.com>
2018-10-29 16:38:08 -0700

    populated database with mock products

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -12,32 +12,32 @@ create table products(
     PRIMARY KEY (item_id)
 );
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Jeans", "Clothing, Shoes & Jewelry", 25.00, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Jeans", "Clothing, Shoes & Jewelry", 25.00, 50);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Electronic Toothbrush", "Beauty and Personal Care", 138.00, 35);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Electronic Toothbrush", "Beauty and Personal Care", 138.00, 35);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Swatch Watch", "Clothing, Shoes & Jewelry", 44.49, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Swatch Watch", "Clothing, Shoes & Jewelry", 44.49, 15);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Toshiba Microwave Oven", "Home & Kitchen", 87.16, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toshiba Microwave Oven", "Home & Kitchen", 87.16, 25);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Toblerone Swiss Milk Chocolate (1 Bar)", "Candy & Chocolate", 2.09, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toblerone Swiss Milk Chocolate (1 Bar)", "Candy & Chocolate", 2.09, 50);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Roget's International Thesaurus, 7th Edition (Paperback)", "Books", 20.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Roget's International Thesaurus, 7th Edition (Paperback)", "Books", 20.00, 20);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Apple iPhone X, Fully Unlocked", "Cell Phones  & Accessories", 1040.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apple iPhone X, Fully Unlocked", "Cell Phones  & Accessories", 1040.00, 5);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Floor Lamp", "Tools & Home Improvement", 74.99, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Floor Lamp", "Tools & Home Improvement", 74.99, 8);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Alarm Clock", "Home & Kitchen", 10.69, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Alarm Clock", "Home & Kitchen", 10.69, 10);
 
-    INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES ("Suave Professionals Shampoo", "Shampoo & Conditioner", 3.99, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Suave Professionals Shampoo", "Shampoo & Conditioner", 3.99, 12);
