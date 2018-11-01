d86a17a07996112f16d8d4b52234a479574f986f
ANS <36783010+answebdev@users.noreply.github.com>
2018-11-01 14:08:38 -0700

    added menu to Manager View

--- bamazonManager.js
+++ bamazonManager.js
@@ -3,6 +3,9 @@ var inquirer = require("inquirer");
 var Table = require('cli-table');
 var colors = require('colors');
 
+// require('events').EventEmitter.prototype._maxListeners = 100;
+// require('events').EventEmitter.defaultMaxListeners = 15;
+
 // Create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -30,75 +33,6 @@ connection.connect(function (err) {
     start();
 });
 
-// function displayTable() {
-//     connection.query("SELECT * FROM products", function (err, res) {
-//         if (err) throw err;
-//         // Instantiate Table
-//         var table = new Table({
-//             head: ['PRODUCT ID', 'PRODUCT NAME', 'DEPARTMENT NAME', 'PRICE', 'NO. AVAILABLE'],
-//             colWidths: [12, 60, 35, 10, 15]
-//         });
-//         for (var i = 0; i < res.length; i++) {
-//             // Create table
-//             var newRow = [res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price.toFixed(2), res[i].stock_quantity];
-//             table.push(newRow);
-//         }
-//         console.log(table.toString());
-//         start();
-//     })
-// }
-
-function start() {
-    inquirer.prompt([
-        {
-            "name": "action",
-            "message": colors.yellow("Welcome to your Bamazon manager console. Please select one of the following:"),
-            "type": "list",
-            "choices": ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
-        }
-    ]).then(function (choice) {
-        switch (choice.action) {
-            case "View Products for Sale":
-                displayTable();
-                break;
-            case "View Low Inventory":
-                viewLowInv();
-                break;
-            case "Add to Inventory":
-                addToInv();
-                break;
-            case "Add New Product":
-                addProduct();
-                break;
-            case "Exit":
-                console.log(colors.cyan("\nThank you for shopping with us. Please come again."))
-                connection.end()
-                break;
-        }
-    })
-}
-
-function viewLowInv() {
-    inquirer.prompt([
-        {
-            "name": "action",
-            "message": colors.yellow("Welcome to Bamazon! What would you like to do?"),
-            "type": "list",
-            "choices": ["Buy an item", "Exit"]
-        }
-    ]).then(function (choice) {
-        switch (choice.action) {
-            case "Buy an item":
-                shop();
-                break;
-            case "Exit":
-                console.log(colors.cyan("\nThank you for shopping with us. Please come again."))
-                connection.end()
-                break;
-        }
-    })
-}
-
 function displayTable() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -115,4 +49,65 @@ function displayTable() {
         console.log(table.toString());
         start();
     })
+}
+
+function start() {
+    inquirer.prompt([
+        {
+            "name": "action",
+            "message": colors.yellow("Welcome to your Bamazon manager console. Please select one of the following:"),
+            "type": "list",
+            "choices": ["View Products for Sale", "View Low Inventory (less than 5 items)", "Add to Inventory", "Add New Product", "Exit"]
+        }
+    ]).then(function (choice) {
+        switch (choice.action) {
+            case "View Products for Sale":
+                displayTable();
+                break;
+            case "View Low Inventory (less than 5 items)":
+                viewLowInv();
+                break;
+            case "Add to Inventory":
+                addToInv();
+                break;
+            case "Add New Product":
+                addProduct();
+                break;
+            case "Exit":
+                console.log(colors.cyan("\nExiting program."))
+                connection.end()
+                break;
+        }
+    })
+}
+
+function viewLowInv() {
+    // console.log("Less then 5 items here");
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ['PRODUCT ID', 'PRODUCT NAME', 'DEPARTMENT NAME', 'PRICE', 'NO. AVAILABLE'],
+            colWidths: [12, 60, 35, 10, 15]
+        })
+
+        for (var i = 0; i < res.length; i++) {
+            // if (res[i].stock_quantity < 5) {
+                var newRow = [res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price.toFixed(2), res[i].stock_quantity];
+                // var newRow = [row.item_id, row.product_name, row.department_name, "$" + row.price.toFixed(2), row.stock_quantity];
+                table.push(newRow);
+                // displayTable();
+            }
+            console.log("\n" + table.toString());
+            start();
+    })
+}
+
+function addToInv() {
+    console.log("Add to inventory");
+    start();
+}
+
+function addProduct() {
+    console.log("Add product");
+    start();
 }
