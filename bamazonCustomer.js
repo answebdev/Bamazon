2d869d84e7d6816a21771f7f3059aa605c30898e
ANS <36783010+answebdev@users.noreply.github.com>
2018-11-01 11:59:00 -0700

    fixed bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -85,7 +85,7 @@ function start() {
             //     // process.on('exit', function() { process.exit(1); });
             //     // process.exit(-1);
             // }
-            if (inquirerResponse.productID > 10) {
+            if ((inquirerResponse.productID) > 10 || (inquirerResponse.productID < 1)) {
                 console.log(colors.cyan("\nPlease enter a valid product ID to continue.\n"));
                 return start();
             }
@@ -94,18 +94,21 @@ function start() {
             // var query = "SELECT item_ID, product_name, department_name, price, stock_quantity FROM products WHERE ?";
             connection.query('SELECT * FROM products WHERE item_id=?', [productID], function (err, res) {
                 if (err) throw err;
+                var product = res[0].product_name;
                 var price = res[0].price.toFixed(2);
                 var orderTotal = 0;
                 for (var i = 0; i < res.length; i++) {
-                    console.log("\nYou have selected product ID " + res[i].item_id + ": " + res[i].product_name);
-                    console.log("Quantity: " + inquirerResponse.quantity);
+                    // console.log("\nYou have selected product ID " + res[i].item_id + ": " + res[i].product_name);
+                    // console.log("Quantity: " + inquirerResponse.quantity);
                     if (inquirerResponse.quantity > res[i].stock_quantity) {
                         console.log(colors.cyan("\nThere are not enough in stock. Please try again.\n"));
                         // console.log(colors.cyan("\nThere are only " + res[i].stock_quantity + " of those in stock. Please try again.\n"));
                         start();
                     } else {
                         connection.query("UPDATE products SET stock_quantity='" + (res[i].stock_quantity - inquirerResponse.quantity) + "' WHERE item_id='" + res[i].item_id + "'", function (err, res) {
-                            console.log(colors.cyan("\nItem added to cart."));
+                            console.log(colors.cyan("\nYour order was placed successfully!"));
+                            console.log(colors.cyan("\nItem: " + product));
+                            console.log(colors.cyan("Quantity: " + inquirerResponse.quantity));
                             console.log(colors.cyan("Your total comes to $" + (price * inquirerResponse.quantity).toFixed(2)) + ".\n");
                             displayTable();
                         })
