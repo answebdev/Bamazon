c5d11208a3af764e526c2a89b2c8389b01600824
ANS <36783010+answebdev@users.noreply.github.com>
2018-11-03 12:28:42 -0700

    fixed validation bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -45,11 +45,11 @@ function displayTable() {
             table.push(newRow);
         }
         console.log(table.toString());
-        start();
+        start(res);
     })
 }
 
-function start() {
+function start(res) {
     inquirer.prompt([
         {
             "name": "action",
@@ -60,7 +60,7 @@ function start() {
     ]).then(function (choice) {
         switch (choice.action) {
             case "Buy an item":
-                shop();
+                shop(res);
                 break;
             case "Exit":
                 console.log(colors.cyan("\nThank you for shopping with us. Please come again."))
@@ -70,7 +70,7 @@ function start() {
     })
 }
 
-function shop() {
+function shop(res) {
     inquirer.prompt([
         {
             name: "productID",
@@ -96,31 +96,32 @@ function shop() {
         }
     ])
         .then(function (inquirerResponse) {
-            // if ((inquirerResponse.productID) > 10 || (inquirerResponse.productID < 1)) {
-            //     console.log(colors.cyan("\nPlease enter a valid product ID to continue.\n"));
-            //     return start();
-            // }
-            var productID = inquirerResponse.productID;
-            var quantity = inquirerResponse.quantity;
-            connection.query('SELECT * FROM products WHERE item_id=?', [productID], function (err, res) {
-                if (err) throw err;
-                var product = res[0].product_name;
-                var price = res[0].price.toFixed(2);
-                var orderTotal = 0;
-                for (var i = 0; i < res.length; i++) {
-                    if (inquirerResponse.quantity > res[i].stock_quantity) {
-                        console.log(colors.cyan("\nThere are not enough in stock. Please try again.\n"));
-                        start();
-                    } else {
-                        connection.query("UPDATE products SET stock_quantity='" + (res[i].stock_quantity - inquirerResponse.quantity) + "' WHERE item_id='" + res[i].item_id + "'", function (err, res) {
-                            console.log(colors.cyan("\nYour order was placed successfully!"));
-                            console.log(colors.cyan("\nItem: " + product));
-                            console.log(colors.cyan("Quantity: " + inquirerResponse.quantity));
-                            console.log(colors.cyan("Your total comes to $" + (price * inquirerResponse.quantity).toFixed(2) + ".\n"));
-                            displayTable();
-                        })
+            if (!((inquirerResponse.productID) < res.length + 1) || (inquirerResponse.productID < 1)) {
+                console.log(colors.cyan("\nPlease enter a valid product ID to continue.\n"));
+                return start(res);
+            } else {
+                var productID = inquirerResponse.productID;
+                var quantity = inquirerResponse.quantity;
+                connection.query('SELECT * FROM products WHERE item_id=?', [productID], function (err, res) {
+                    if (err) throw err;
+                    var product = res[0].product_name;
+                    var price = res[0].price.toFixed(2);
+                    var orderTotal = 0;
+                    for (var i = 0; i < res.length; i++) {
+                        if (inquirerResponse.quantity > res[i].stock_quantity) {
+                            console.log(colors.cyan("\nThere are not enough in stock. Please try again.\n"));
+                            start(res);
+                        } else {
+                            connection.query("UPDATE products SET stock_quantity='" + (res[i].stock_quantity - inquirerResponse.quantity) + "' WHERE item_id='" + res[i].item_id + "'", function (err, res) {
+                                console.log(colors.cyan("\nYour order was placed successfully!"));
+                                console.log(colors.cyan("\nItem: " + product));
+                                console.log(colors.cyan("Quantity: " + inquirerResponse.quantity));
+                                console.log(colors.cyan("Your total comes to $" + (price * inquirerResponse.quantity).toFixed(2) + ".\n"));
+                                displayTable();
+                            })
+                        }
                     }
-                }
-            })
+                })
+            }
         })
 }
